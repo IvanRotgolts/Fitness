d3452a35a7a99801f19a65c29153bcde032ab6a6
Inter <rotgolts.ivan@gmail.com>
2024-02-07 18:51:52 +0300

    добавил csvParcer

--- csv_parcer.py
+++ csv_parcer.py
@@ -3,9 +3,9 @@ from quiz import QuizItem
 class QuizCSVParcer:
     """класс описывает csv_parcer"""
 
-    def __init__(self, file_path, separator):
-        self.file_path = file_path
-        self.separator = separator
+    def __init__(self):
+        self.file_path = "questions.csv"
+        self.separator = ";"
         self.items = []
 
     def parce_line(self, line):
@@ -15,18 +15,19 @@ class QuizCSVParcer:
          right_answer = columns[1]
          answers = columns[2:]
 
-         return QuizItem(question = question, right_answer=right_answer, all_answers=answers)
+         return QuizItem(question = question, right_answer = right_answer, all_answers = answers)
     
     def parce_lines(self):
         """делает разбор целого файла и возвращает объекты QuizItem"""
 
         lines = []
 
+        #Открытие файла на чтение
         with open(self.file_path, "r", encoding = "utf-8") as file:
             lines=file.readlines()
-
+        #Перебор прочитанных линий из файла
         for line in lines:
             self.items.append(self.parce_line(line)) 
 
         return self.items
-       
+
